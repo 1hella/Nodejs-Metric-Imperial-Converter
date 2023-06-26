40ebee14e068d86f3b144b21cd4ce86b58d23229
Stephen Wanhella <stephen.wanhella@gmail.com>
2023-06-26 14:19:48 -0700

    fix functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,22 +5,38 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite('Functional Tests', function () {
     this.timeout(5000);
-    test('Convert a valid input', function(done) {
+    test('Convert a valid input', function (done) {
         chai
-        .request(server)
-        .keepOpen()
-        .get('/api/convert?input=10L')
-        .end((err, res) => {
-            console.log(res.body);
-            assert.equal(res.status, 200);
-            assert.equal(res.body.initNum, 10);
-            assert.equal(res.body.initUnit, 'L');
-            assert.equal(res.body.returnNum, 2.64172);
-            assert.equal(res.body.returnUnit, 'gal');
-            assert.equal(res.body.string, "10 liters converts to 2.64172 gallons");
-            done();
-        });
+            .request(server)
+            .keepOpen()
+            .get('/api/convert?input=10L')
+            .end((err, res) => {
+                console.log(res.body);
+                assert.equal(res.status, 200);
+                assert.equal(res.body.initNum, 10);
+                assert.equal(res.body.initUnit, 'L');
+                assert.equal(res.body.returnNum, 2.64172);
+                assert.equal(res.body.returnUnit, 'gal');
+                assert.equal(res.body.string, "10 liters converts to 2.64172 gallons");
+                done();
+            });
+    });
+    test('Convert an invalid input', function (done) {
+        chai
+            .request(server)
+            .keepOpen()
+            .get('/api/convert?input=32g')
+            .end((err, res) => {
+                console.log(res.body);
+                assert.equal(res.status, 200);
+                assert.equal(res.body.initNum, 10);
+                assert.equal(res.body.initUnit, 'L');
+                assert.equal(res.body.returnNum, 2.64172);
+                assert.equal(res.body.returnUnit, 'gal');
+                assert.equal(res.body.string, "10 liters converts to 2.64172 gallons");
+                done();
+            });
     });
 });
